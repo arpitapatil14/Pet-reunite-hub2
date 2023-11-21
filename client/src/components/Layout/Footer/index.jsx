ffe8f68823fce317b99a0bbda38f2cd5d111d60c
meetmovaliya <meet.movaliya2021@vitstudent.ac.in>
2023-11-21 11:57:24 +0530

    first commit

---
+++ client/src/components/Layout/Footer/index.jsx
@@ -0,0 +1,12 @@
+import './style.css';
+
+const Footer = () => {
+  return (
+    <footer className="general_footer">
+      <span className="copyright">Forum, {new Date().getFullYear()}. ALL RIGHTS RECEIVED</span>
+      
+    </footer>
+  )
+}
+
+export default Footer;
